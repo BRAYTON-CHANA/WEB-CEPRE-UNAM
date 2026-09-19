2a7a277e6b971c5578a63a3cb0befaa6031aa110
BRAYTON CHANA <brayton-leonardo-12@hotmail.com>
2026-09-19 09:04:54 -0500

    asistencia 3

--- frontend/src/features/layout/CepreLayout.jsx
+++ frontend/src/features/layout/CepreLayout.jsx
@@ -16,7 +16,7 @@ const CepreLayout = ({ children, showHeader = true, showFooter = true, showSideb
 
   if (showSidebar) {
     return (
-      <LayoutWithSidebar header={header} footer={footer} sidebar={CepreSidebar} defaultOpen={true} defaultPinned={true}>
+      <LayoutWithSidebar header={header} footer={footer} sidebar={CepreSidebar} defaultOpen={true} defaultPinned={false}>
         {children}
       </LayoutWithSidebar>
     );
